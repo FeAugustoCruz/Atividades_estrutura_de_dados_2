14f660a052fdb102449f6c694659aff04c955d1b
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-09-24 02:33:21 -0300

    commit

--- att1.c
+++ att1.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+int BuscaStringConst(const char *texto, const char *padrao) {
+    int lenTexto = strlen(texto);
+    int lenPadrao = strlen(padrao);
+
+    if (lenPadrao > lenTexto) {
+        return -2; // erro de tamanho
+    }
+
+    for (int i = 0; i <= lenTexto - lenPadrao; i++) {
+        int j = 0;
+        for (j = 0; j < lenPadrao; j++) {
+            if (texto[i + j] != padrao[j]) {
+                break;
+            }
+        }
+        if (j == lenPadrao) {
+            return i; // posição da ocorrência
+        }
+    }
+
+    return -1; // não encontrada
+}
 
 int main(void){
+    printf("%d",BuscaStringConst("abtestcef", "teste"));
 
 
     return 0;
--- att2.c
+++ att2.c
@@ -2,30 +2,16 @@
 
 #define LEN 10
 
-int BuscaBinariaRec(int vet[], int v, int esquerda, int direita){
-    int esq = esquerda;
-    int dir = direita;
-
-    int meio = (esq + dir)/2;
-
-    if (esq + dir){
+int BuscaBinariaRec(int vet[], int v, int ini, int fim){
+    int meio = (ini + fim)/2;
+    printf("--%d--\n", meio);
+    if (ini > fim){
         return -1;
-    }else if(v = vet[meio]){
-        return BuscaBinariaRec(vet, v, esq, dir);
-    }else{
-        
+    }else if(v == vet[meio]){
+        return BuscaBinariaRec(vet, v, ini, meio - 1);
+    }else if (v < vet[meio]){
+        return BuscaBinariaRec(vet, v, ini, meio - 1);    
     }
-
-    if (v < vet[meio] ){
-        dir = meio - 1;
-        BuscaBinariaRec(vet, v, esq, dir);
-    }else if (v > vet[meio]){
-        esq = meio + 1;
-        BuscaBinariaRec(vet, v, esq, dir);
-    }else{
-        return vet[meio];
-    }
-    return -1;
 }
 
 int BuscaBinaria(int vet[], int v){
