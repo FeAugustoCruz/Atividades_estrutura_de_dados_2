0449f26372c487b0cb0c4295aba65bf7610d7560
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-09-29 11:49:28 -0300

    commit força bruta

---
+++ att3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int ForcaBruta(const char* t, int n, const char* p, int m){
+    for(int i = 0; (n - m) > i; i++){
+        printf("Teste2\n");
+        int j =0;
+        while(j < m && (t[i+j] == p[j])){
+            j += 1;
+            if(j == m){
+                return i;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(void){
+    printf("Teste1\n");
+    printf("-> %d", ForcaBruta("teste", strlen("teste"), "st", strlen("ola")));
+
+
+    return 0;
+}
